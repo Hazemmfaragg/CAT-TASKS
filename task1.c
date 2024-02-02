eae7fe4af2429699154d4e927d019cb9e2a272b4
Hazemmfaragg <158563811+Hazemmfaragg@users.noreply.github.com>
2024-02-02 13:55:50 +0200

    Add files via upload

---
+++ task1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+   
+    printf("NAME:Hazem Mahmoud Farag\nBIRTH YEAR:2004\nFACULTY:Engineering\nGraduation Year:2027");
+
+    return 0;
+}
---
+++ task3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  printf("Enter an amount:");
+  double x;
+   double z;
+  scanf("%lf",&x);
+  z = x*1.05 ;
+    printf("%lf",z);
+
+    return 0;
+}
